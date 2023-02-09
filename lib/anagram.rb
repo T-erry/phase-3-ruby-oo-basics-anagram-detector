0cf417eb6d18385edf46bda8921bcc084851f6c2
T-erry <terrywachuka10@gmail.com>
2023-02-09 17:01:22 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(possible_anagrams)
+      possible_anagrams.select { |p| anagram?(p) }
+    end
+  
+    def anagram?(possible_anagram)
+      possible_anagram != @word && possible_anagram.chars.sort == @word.chars.sort
+    end
+  end
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
+
